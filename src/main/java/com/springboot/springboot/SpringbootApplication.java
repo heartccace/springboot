507b079391119bd7d0831f346e6b7dbcd973b342
heartccace <heartccace@gmail.com>
2020-10-21 21:52:59 +0800

    add

--- src/main/java/com/springboot/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/springboot/SpringbootApplication.java
@@ -2,10 +2,11 @@ package com.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+    private RedisTemplate template;
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
